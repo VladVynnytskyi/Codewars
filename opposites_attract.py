74e852a5831bfef9fdec491d4b745dee842e355d
Vladyslav Vynnytskyi <vynnytskyivladyslav@gmail.com>
2025-08-29 22:53:08 +0200

    done

---
+++ opposites_attract.py
@@ -0,0 +1,17 @@
+def lovefunc( flower1, flower2 ):
+    if flower1 % 2 == 0 and flower2 % 2 == 0:
+        return False
+    elif flower1 % 2 != 0 and flower2 % 2 != 0:
+        return False
+    else:
+        return True
+
+
+#or
+
+def lovefunc( flower1, flower2 ):
+    if flower1 % 2 == 0 and flower2 % 2 != 0 or flower1 % 2 != 0 and flower2 % 2 == 0:
+        return True
+    else:
+        return False
+
