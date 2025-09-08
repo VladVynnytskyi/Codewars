f5036eb865e876cab57d6f4f0c4724f2198bc53a
Vladyslav Vynnytskyi <vynnytskyivladyslav@gmail.com>
2025-09-08 22:08:08 +0200

    done

---
+++ 7kyu/Exes and Ohs.py
@@ -0,0 +1,20 @@
+def xo(s):
+    s = s.lower()
+    result_o = 0
+    result_x = 0
+    
+    for char in s:
+        if 'o' in char:
+            result_o += 1
+        if 'x' in char:
+            result_x += 1
+    if result_o == result_x:
+        return True
+    else :
+        return False
+        
+def xo(s):
+    s = s.lower()  # ігноруємо регістр
+    count_o = s.count('o')
+    count_x = s.count('x')
+    return count_o == count_x
